679dd2e43f45bd8c2a290999003ffcd62acdf2ef
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2022-11-08 13:50:43 +0100

    Refactor rust code

--- rust-jni/src/lib.rs
+++ rust-jni/src/lib.rs
@@ -1,21 +1,17 @@
 #![warn(clippy::all)]
 #![allow(clippy::missing_safety_doc)]
 
-use curve25519_dalek::edwards::CompressedEdwardsY;
-use curve25519_dalek::ristretto::{RistrettoPoint, CompressedRistretto};
+
+use std::error::Error;
+
+use curve25519_dalek::ristretto::CompressedRistretto;
 use jni::sys::{jlong, jobject, jboolean};
-use jni::{JNIEnv, sys::jbyteArray};
+use jni::JNIEnv;
 use jni::objects::{JClass, JValue, JObject};
 use merlin::Transcript;
 use curve25519_dalek::scalar::Scalar;
 use bulletproofs::{BulletproofGens, PedersenGens, RangeProof};
 use rand::thread_rng;
-
-fn wrap_to_object<'a>(env : JNIEnv<'a>, bytes : &[u8], class : &str) -> JObject<'a> {
-    let commit = env.byte_array_from_slice(bytes).unwrap();
-    let commit = unsafe {JValue::Object(JObject::from_raw(commit))};
-    env.new_object(class, "", &[commit]).unwrap()
-}
 
 
 const TRANSSCRIPT_LABEL : &[u8] = b"";
@@ -32,6 +28,23 @@ pub unsafe extern "system" fn Java_dk_alexandra_bulletproofcoffee_FFI_prove(
     _jclass : JClass,
     secret : jlong,
 ) -> jobject {
+    match prove(env, secret) {
+        Ok(res) => *res,
+        Err(_) => {
+            env.throw_new(BULLET_PROOF_EXCEPTION_CLASS, "Error constructing rangeproof").unwrap();
+            *JObject::null()
+        }
+    }
+}
+
+fn bytes_to_jobject<'a>(env : &'a JNIEnv, class : &str, bytes : &'a [u8]) -> Result<JObject<'a>, Box<dyn Error>> {
+    let object = env.byte_array_from_slice(bytes)?;
+    let object = unsafe { JValue::Object(JObject::from_raw(object)) };
+    let object = env.new_object(class, "([B)V", &[object])?;
+    Ok(object)
+}
+
+fn prove(env : JNIEnv, secret: jlong) -> Result<JObject, Box<dyn Error>> {
     let secret = secret.unsigned_abs();
 
     let pc_gens = PedersenGens::default();
@@ -45,21 +58,14 @@ pub unsafe extern "system" fn Java_dk_alexandra_bulletproofcoffee_FFI_prove(
         secret,
         &blinding,
         32,
-    ).unwrap();
-
-    // let _ = env.throw_new("dk.alexandra.bulletproofcoffee.BulletProofException", "Could not create proof.");
+    )?;
 
     let proof = proof.to_bytes();
-    let proof = env.byte_array_from_slice(&proof).unwrap();
-
+    let proof = bytes_to_jobject(&env, PROOF_CLASS, &proof)?;
     let commit = committed_value.to_bytes();
-    let commit = env.byte_array_from_slice(&commit).unwrap();
-
-    let commit = JValue::Object(JObject::from_raw(commit));
-    let commit = env.new_object(COMMITMENT_CLASS, "([B)V", &[commit]).unwrap();
-    let proof = JValue::Object(JObject::from_raw(proof));
-    let proof = env.new_object(PROOF_CLASS, "([B)V", &[proof]).unwrap();
-    *env.new_object(PAIR_CLASS, "(Ljava/lang/Object;Ljava/lang/Object;)V", &[proof.into(), commit.into()]).unwrap()
+    let commit = bytes_to_jobject(&env, COMMITMENT_CLASS, &commit)?;
+    let pair = env.new_object(PAIR_CLASS, "(Ljava/lang/Object;Ljava/lang/Object;)V", &[proof.into(), commit.into()])?;
+    Ok(pair)
 }
 
 
@@ -71,21 +77,36 @@ pub unsafe extern "system" fn Java_dk_alexandra_bulletproofcoffee_FFI_verify(
     proof : jobject,
     commit : jobject,
 ) -> jboolean {
-    let proof = JObject::from_raw(proof);
-    let proof = env.call_method(proof, "asBytes", "()[B", &[]).unwrap();
-    let proof = env.convert_byte_array(*proof.l().unwrap()).unwrap();
-    let proof = RangeProof::from_bytes(&proof).unwrap();
+    match verify(env, proof, commit) {
+        Ok(res) => res,
+        Err(_) => {
+            env.throw_new(BULLET_PROOF_EXCEPTION_CLASS, "Error verifying rangeproof").unwrap();
+            0
+        }
+    }
+}
 
-    let commit = JObject::from_raw(commit);
-    let commit = env.call_method(commit, "asBytes", "()[B", &[]).unwrap();
-    let commit = env.convert_byte_array(*commit.l().unwrap()).unwrap();
+fn jobject_as_bytes(env : JNIEnv, object : jobject) -> Result<Vec<u8>, Box<dyn Error>> {
+    let object = unsafe {JObject::from_raw(object) };
+    let bytes = env.call_method(object, "asBytes", "()[B", &[])?;
+    let bytes = env.convert_byte_array(*bytes.l()?)?;
+    Ok(bytes)
+
+}
+
+fn verify(env : JNIEnv, proof: jobject, commit : jobject) -> Result<jboolean, Box<dyn Error>>{
+    let proof = jobject_as_bytes(env, proof)?;
+    let proof = RangeProof::from_bytes(&proof)?;
+
+    let commit = jobject_as_bytes(env, commit)?;
     let commit = CompressedRistretto::from_slice(&commit);
 
     let pc_gens = PedersenGens::default();
     let bp_gens = BulletproofGens::new(64, 1);
 
     let mut verifier_transcript = Transcript::new(TRANSSCRIPT_LABEL);
-    proof.verify_single(&bp_gens, &pc_gens, &mut verifier_transcript, &commit, 32).is_ok().into()
+    let check = proof.verify_single(&bp_gens, &pc_gens, &mut verifier_transcript, &commit, 32);
+    Ok(check.is_ok().into())
 }
 
 
