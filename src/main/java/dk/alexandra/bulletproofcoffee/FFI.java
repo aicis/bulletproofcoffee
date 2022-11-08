ca57a917b42534f9e9be21408d154f5a8d2c9a7b
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2022-11-08 14:41:18 +0100

    Add javadoc for native methods

--- src/main/java/dk/alexandra/bulletproofcoffee/FFI.java
+++ src/main/java/dk/alexandra/bulletproofcoffee/FFI.java
@@ -12,8 +12,20 @@ public class FFI {
         System.load(lib.getAbsolutePath());
     }
 
+    /**
+     * Construct a range proof
+     * @param secret that lies in range 0 <= secret <= bound
+     * @param bound upper bound `n`, must be a power of 2
+     * @return a pair consisting of a proof and commitment
+     */
     public native static Pair<Proof, Commitment> proveRange(long secret, int bound);
 
+    /**
+     *  Verify a range proof
+     * @param proof that is to be verified
+     * @param commitment used for verifying
+     * @return true if the proof is valid
+     */
     public native static boolean verify(Proof proof, Commitment commitment);
 
 }
